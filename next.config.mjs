a77b85018c628418b61683e4b91dfce1d389a380
developergokselglobal <developer@gokselglobal.com>
2024-03-11 19:34:04 +0300

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    distDir: 'out',   
+};
 
 export default nextConfig;
