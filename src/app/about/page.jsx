9cda0635a755d14e5e65b8f999d5ad65267de529
developergokselglobal <developer@gokselglobal.com>
2024-03-11 21:13:17 +0300

    deploy

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -151,10 +151,11 @@ function Team() {
                       <FadeIn>
                         <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                           <Image
+                            unoptimized
                             alt=""
                             {...person.image}
                             className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                            unoptimized
+                            
                           />
                           <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
                             <p className="font-display text-base/6 font-semibold tracking-wide text-white">
